cc4d160f9fff6eb890cd8ec7c5ac2a5aefd70ba4
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2023-09-05 22:53:35 -0400

    Starting from scratch to find my mistakes and problem

--- js/script.js
+++ js/script.js
@@ -1,41 +1,17 @@
 // Assignment code here
-var options = document.querySelectorAll(".options input[type=checkbox]")
-const characters = { //object and letters
-  Lowercase:"abcdefghijklmnopqrstuvwxyz",
-  Uppercase:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  Numbers:"123456789",
-  Special:"@#$%&!_&"
-}
-let password;
-const generatepassword = () => {
-  password = " ";
-  options.forEach(div => {
-    if(document.querySelector("input").checked) {
-      password += characters[div.parentElement.id]
-    }
-  }
-  )
-}
 
-function test(question){
-  let answer=prompt("Are you a grader? Yes or No");
-  if(answer.toLowerCase() === "yes"){
-    alert("Welcome Grader")
-    return true;
-  } else if (answer.toLowerCase() === "no"){
-    alert("You were trying to cross the border, right? Walked right into that Imperial ambush, same as us, and that thief over there! (Skyrim Reference)")
-    return false;
-  } else{
-    alert("Invalid Answer");
-    return(question);
-  }
-}
 
-test();
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click",generatepassword);
+generateBtn.addEventListener("click", writePassword);
