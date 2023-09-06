85e89a67a28fc96b93fb3d9b10e3e99f0f20114b
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2023-09-05 23:11:45 -0400

    Work in progress,so far so good

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 // Assignment code here
+var lengthofpassword = 8;
+var choice = [];
+
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","y","Z"];
+var lowercase= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var special=["!","@","#","$","*","$","&","_","?"];
+var numbers=["1","2","3","4","5","6","7","8","9"];
+function generatePassword() {
+  console.log("test");
+
+  return "Working";
+}
+
+function generatePassword(){
+
+}
 
 
+function popup(){
+  lengthofpassword.length = parseInt(prompt("Choose between 8-25"))
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
